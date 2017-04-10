ab2522afbf08f1f797c5d8e389d06a37a1ea33c7
Kirill Osenkov <github@osenkov.com>
2017-04-09 22:32:55 -0700

    Add license header to two more files.

--- PEFile/ImageReaderWithCommentedOutCode.cs
+++ PEFile/ImageReaderWithCommentedOutCode.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿//
+// Author:
+//   Jb Evain (jbevain@gmail.com)
+//
+// Copyright (c) 2008 - 2015 Jb Evain
+// Copyright (c) 2008 - 2011 Novell, Inc.
+//
+// Licensed under the MIT/X11 license.
+//
+
+using System;
 using System.IO;
 using System.Linq;
 using RVA = System.UInt32;
--- PEFile/ReadHeaders.cs
+++ PEFile/ReadHeaders.cs
@@ -1,4 +1,14 @@
-﻿using System.IO;
+﻿//
+// Author:
+//   Jb Evain (jbevain@gmail.com)
+//
+// Copyright (c) 2008 - 2015 Jb Evain
+// Copyright (c) 2008 - 2011 Novell, Inc.
+//
+// Licensed under the MIT/X11 license.
+//
+
+using System.IO;
 
 namespace PEFile
 {
