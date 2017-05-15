066c8b6fcc84d0041135ec5bfa2f8e8d0cb38d95
Kirill Osenkov <github@osenkov.com>
2017-05-15 13:40:41 -0700

    Include a method to read the assembly version using SRM.

---
+++ PEFile/GetAssemblyVersion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+
+namespace PEFile
+{
+    public static class FileUtilities
+    {
+        public static Version GetAssemblyVersion(string sourcePath)
+        {
+            using (var assemblyStream = new FileStream(sourcePath, FileMode.Open, FileAccess.Read, FileShare.Delete | FileShare.Read))
+            {
+                Version result = null;
+                try
+                {
+                    using (PEReader peReader = new PEReader(assemblyStream, PEStreamOptions.LeaveOpen))
+                    {
+                        if (peReader.HasMetadata)
+                        {
+                            MetadataReader reader = peReader.GetMetadataReader();
+                            if (reader.IsAssembly)
+                            {
+                                result = reader.GetAssemblyDefinition().Version;
+                            }
+                        }
+                    }
+                }
+                catch (BadImageFormatException)
+                {
+                    // not a PE
+                }
+
+                return result;
+            }
+        }
+    }
+}
