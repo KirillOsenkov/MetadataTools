f819532d727f7662aa543550923f6f7638fe1f84
Kirill Osenkov <github@osenkov.com>
2017-11-21 21:50:12 -0800

    Add a way to dump XML with typerefs and memberrefs.

--- RefDump/Program.cs
+++ RefDump/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 using Mono.Cecil;
 
 namespace RefDump
@@ -11,6 +12,7 @@ namespace RefDump
         private string[] args;
 
         public string FilePath { get; private set; }
+        public string OutputXml { get; set; }
 
         public Dumper(string[] args)
         {
@@ -48,7 +50,9 @@ namespace RefDump
             {
                 InMemory = true
             };
+
             var assemblyDefinition = AssemblyDefinition.ReadAssembly(FilePath, readerParameters);
+
             Log(assemblyDefinition.Name.FullName);
             Log();
             Log("References:", ConsoleColor.Green);
@@ -86,6 +90,125 @@ namespace RefDump
                     PrintWithHighlight(text, text.LastIndexOf('.'), ConsoleColor.Gray, ConsoleColor.White);
                 }
             }
+
+            if (OutputXml != null)
+            {
+                var refTree = GetRefTree(assemblyDefinition);
+                DumpToXml(refTree, OutputXml);
+            }
+        }
+
+        private void DumpToXml(RefTree refTree, string outputXml)
+        {
+            var document = new XDocument();
+            document.Add(new XElement("Assembly"));
+            foreach (var asm in refTree.Assemblies.OrderBy(a => a.Key))
+            {
+                Dump(document.Root, asm);
+            }
+
+            document.Save(OutputXml);
+        }
+
+        private void Dump(XElement root, KeyValuePair<string, RefAssembly> asm)
+        {
+            var referenceElement = new XElement("Reference");
+            referenceElement.SetAttributeValue("Name", asm.Key);
+            foreach (var type in asm.Value.Types.OrderBy(t => t.Key))
+            {
+                Dump(referenceElement, type);
+            }
+
+            root.Add(referenceElement);
+        }
+
+        private void Dump(XElement referenceElement, KeyValuePair<string, RefType> type)
+        {
+            var typeElement = new XElement("Type");
+            typeElement.SetAttributeValue("Name", type.Key);
+            foreach (var item in type.Value.Members.OrderBy(m => m.FullName))
+            {
+                Dump(typeElement, item);
+            }
+
+            referenceElement.Add(typeElement);
+        }
+
+        private void Dump(XElement typeElement, MemberReference memberReference)
+        {
+            var memberElement = new XElement("Member");
+            memberElement.SetAttributeValue("Name", memberReference.Name);
+            memberElement.SetAttributeValue("FullName", memberReference.FullName);
+            typeElement.Add(memberElement);
+        }
+
+        class RefTree
+        {
+            public Dictionary<string, RefAssembly> Assemblies { get; set; } = new Dictionary<string, RefAssembly>();
+
+            public void AddMember(MemberReference memberReference)
+            {
+                var typeReference = memberReference.DeclaringType;
+                var assembly = typeReference.Scope.ToString() ?? "<null>";
+
+                if (!Assemblies.TryGetValue(assembly, out var refAssembly))
+                {
+                    refAssembly = new RefAssembly();
+                    Assemblies[assembly] = refAssembly;
+                }
+
+                refAssembly.AddMember(memberReference);
+            }
+        }
+
+        class RefAssembly
+        {
+            public Dictionary<string, RefType> Types { get; set; } = new Dictionary<string, RefType>();
+
+            public void AddMember(MemberReference memberReference)
+            {
+                var typeReference = memberReference.DeclaringType;
+
+                if (!Types.TryGetValue(typeReference.FullName, out var typeRef))
+                {
+                    typeRef = new RefType();
+                    Types[typeReference.FullName] = typeRef;
+                }
+
+                typeRef.AddMember(memberReference);
+            }
+        }
+
+        class RefType
+        {
+            public List<MemberReference> Members { get; set; } = new List<MemberReference>();
+
+            public void AddMember(MemberReference memberReference)
+            {
+                Members.Add(memberReference);
+            }
+        }
+
+        private RefTree GetRefTree(AssemblyDefinition assemblyDefinition)
+        {
+            var refTree = new RefTree();
+
+            foreach (var memberReference in assemblyDefinition.MainModule.GetMemberReferences())
+            {
+                if (memberReference.DeclaringType.IsArray)
+                {
+                    continue;
+                }
+
+                if (memberReference.DeclaringType.Scope.MetadataScopeType != MetadataScopeType.AssemblyNameReference)
+                {
+                    continue;
+                }
+
+                refTree.AddMember(memberReference);
+            }
+
+            return refTree;
         }
 
         private void PrintWithHighlight(string originalString, int splitPosition, ConsoleColor firstPart, ConsoleColor secondPart)
@@ -103,7 +226,7 @@ namespace RefDump
             }
         }
 
-        private void Log(string text = "", ConsoleColor color = ConsoleColor.Gray, bool lineBreak = true)
+        private static void Log(string text = "", ConsoleColor color = ConsoleColor.Gray, bool lineBreak = true)
         {
             Console.ForegroundColor = color;
             if (lineBreak)
@@ -126,15 +249,26 @@ namespace RefDump
                     arg.EndsWith(".exe", StringComparison.OrdinalIgnoreCase)) && File.Exists(arg))
                 {
                     FilePath = Path.GetFullPath(arg);
+                    continue;
+                }
+
+                if (arg.EndsWith(".xml", StringComparison.OrdinalIgnoreCase))
+                {
+                    OutputXml = Path.GetFullPath(arg);
+                    continue;
                 }
             }
         }
 
         private static void PrintUsage()
         {
-            Console.WriteLine(@"Usage: refdump file.dll
-    Lists all references of the input assembly, 
-    all types and all members consumed from each reference.");
+            Log(@"Usage:", ConsoleColor.Green, lineBreak: false);
+            Log(@"refdump file.dll [output.xml]", ConsoleColor.White);
+
+    Log(@"    Lists all references of the input assembly, 
+    all types and all members consumed from each reference.
+    If an output.xml file name is specified, dump detailed 
+    report into that xml.", ConsoleColor.Gray);
         }
     }
 }
