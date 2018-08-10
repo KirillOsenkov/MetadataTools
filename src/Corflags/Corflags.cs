7c49c49d1a34679810c5f41ead8f6def025e56b8
Kirill Osenkov <github@osenkov.com>
2018-08-09 18:46:17 -0700

    Add corflags project and executable.

---
+++ src/Corflags/Corflags.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Reflection.PortableExecutable;
+
+namespace PEFile
+{
+    public class Corflags
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2 || !File.Exists(args[0]) || args[1] != "/32BITPREF-")
+            {
+                Console.WriteLine("Usage: corflags Assembly [options]");
+                Console.WriteLine("Options:");
+                Console.WriteLine("/32BITPREF-");
+                return;
+            }
+
+            Set32BitPreferredSRM(args[0], prefer32Bit: false);
+        }
+
+        public static void Set32BitPreferredSRM(string filePath, bool prefer32Bit)
+        {
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
+            {
+                PEReader reader = new PEReader(stream);
+
+                CorFlags attributes = reader.PEHeaders.CorHeader.Flags;
+
+                var old = (attributes & CorFlags.Prefers32Bit) == CorFlags.Prefers32Bit;
+                if (old != prefer32Bit)
+                {
+                    if (prefer32Bit)
+                    {
+                        attributes = attributes | CorFlags.Prefers32Bit;
+                    }
+                    else
+                    {
+                        attributes = attributes & ~CorFlags.Prefers32Bit;
+                        attributes = attributes & ~CorFlags.Requires32Bit;
+                    }
+
+                    var offset = reader.PEHeaders.CorHeaderStartOffset;
+                    offset += 0x10; // flags offset
+                    stream.Position = offset;
+                    int value = (int)attributes;
+                    stream.WriteByte((byte)(value & 0xFF));
+                    stream.WriteByte((byte)(value & 0xFF00));
+                    stream.WriteByte((byte)(value & 0xFF0000));
+                    stream.WriteByte((byte)(value & 0xFF000000));
+                }
+            }
+        }
+    }
+}
