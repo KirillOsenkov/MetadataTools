37def85a7a98c65ea44c72cd39f74b7fae7e2e71
Kirill Osenkov <github@osenkov.com>
2020-05-20 21:18:50 -0700

    Detect mismatching interface members and base members.

--- src/BinaryCompatChecker/Program.cs
+++ src/BinaryCompatChecker/Program.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using Mono.Cecil;
+using Mono.Collections.Generic;
 
 namespace BinaryCompatChecker
 {
@@ -589,11 +590,416 @@ namespace BinaryCompatChecker
             CheckTypes(referencing, referenced);
         }
 
+        public static MethodDefinition GetMethod(Collection<MethodDefinition> methods, MethodReference reference)
+        {
+            for (int i = 0; i < methods.Count; i++)
+            {
+                var method = methods[i];
+
+                string methodName = method.Name;
+                int dot = methodName.LastIndexOf('.');
+                if (dot > 0)
+                {
+                    methodName = methodName.Substring(dot + 1);
+                }
+
+                if (methodName != reference.Name)
+                    continue;
+
+                if (method.HasGenericParameters != reference.HasGenericParameters)
+                    continue;
+
+                if (method.HasGenericParameters && method.GenericParameters.Count != reference.GenericParameters.Count)
+                    continue;
+
+                if (!AreSame(method.ReturnType, reference.ReturnType))
+                    continue;
+
+                if (IsVarArg(method) != IsVarArg(reference))
+                    continue;
+
+                if (IsVarArg(method) && IsVarArgCallTo(method, reference))
+                    return method;
+
+                if (method.HasParameters != reference.HasParameters)
+                    continue;
+
+                if (!method.HasParameters && !reference.HasParameters)
+                    return method;
+
+                if (!AreSame(method.Parameters, reference.Parameters))
+                    continue;
+
+                return method;
+            }
+
+            return null;
+        }
+
+        public static bool IsVarArg(IMethodSignature self)
+        {
+            return self.CallingConvention == MethodCallingConvention.VarArg;
+        }
+
+        static bool AreSame(Collection<ParameterDefinition> a, Collection<ParameterDefinition> b)
+        {
+            var count = a.Count;
+
+            if (count != b.Count)
+                return false;
+
+            if (count == 0)
+                return true;
+
+            for (int i = 0; i < count; i++)
+                if (!AreSame(a[i].ParameterType, b[i].ParameterType))
+                    return false;
+
+            return true;
+        }
+
+        static bool IsVarArgCallTo(MethodDefinition method, MethodReference reference)
+        {
+            if (method.Parameters.Count >= reference.Parameters.Count)
+                return false;
+
+            if (GetSentinelPosition(reference) != method.Parameters.Count)
+                return false;
+
+            for (int i = 0; i < method.Parameters.Count; i++)
+                if (!AreSame(method.Parameters[i].ParameterType, reference.Parameters[i].ParameterType))
+                    return false;
+
+            return true;
+        }
+
+        public static int GetSentinelPosition(IMethodSignature self)
+        {
+            if (!self.HasParameters)
+                return -1;
+
+            var parameters = self.Parameters;
+            for (int i = 0; i < parameters.Count; i++)
+                if (parameters[i].ParameterType.IsSentinel)
+                    return i;
+
+            return -1;
+        }
+
+        static bool AreSame(TypeSpecification a, TypeSpecification b)
+        {
+            if (!AreSame(a.ElementType, b.ElementType))
+                return false;
+
+            if (a.IsGenericInstance)
+                return AreSame((GenericInstanceType)a, (GenericInstanceType)b);
+
+            if (a.IsRequiredModifier || a.IsOptionalModifier)
+                return AreSame((IModifierType)a, (IModifierType)b);
+
+            if (a.IsArray)
+                return AreSame((ArrayType)a, (ArrayType)b);
+
+            return true;
+        }
+
+        static bool AreSame(ArrayType a, ArrayType b)
+        {
+            if (a.Rank != b.Rank)
+                return false;
+
+            // TODO: dimensions
+
+            return true;
+        }
+
+        static bool AreSame(IModifierType a, IModifierType b)
+        {
+            return AreSame(a.ModifierType, b.ModifierType);
+        }
+
+        static bool AreSame(GenericInstanceType a, GenericInstanceType b)
+        {
+            if (a.GenericArguments.Count != b.GenericArguments.Count)
+                return false;
+
+            for (int i = 0; i < a.GenericArguments.Count; i++)
+                if (!AreSame(a.GenericArguments[i], b.GenericArguments[i]))
+                    return false;
+
+            return true;
+        }
+
+        static bool AreSame(GenericParameter a, GenericParameter b)
+        {
+            return a.Position == b.Position;
+        }
+
+        static bool AreSame(TypeReference a, TypeReference b)
+        {
+            if (ReferenceEquals(a, b))
+                return true;
+
+            if (a == null || b == null)
+                return false;
+
+            if (a.MetadataType != b.MetadataType)
+                return false;
+
+            if (a.IsGenericParameter)
+                return AreSame((GenericParameter)a, (GenericParameter)b);
+
+            if (IsTypeSpecification(a))
+                return AreSame((TypeSpecification)a, (TypeSpecification)b);
+
+            if (a.Name != b.Name || a.Namespace != b.Namespace)
+                return false;
+
+            //TODO: check scope
+
+            return AreSame(a.DeclaringType, b.DeclaringType);
+        }
+
+        public static bool IsTypeSpecification(TypeReference type)
+        {
+            switch ((ElementType)type.MetadataType)
+            {
+                case ElementType.Array:
+                case ElementType.ByRef:
+                case ElementType.CModOpt:
+                case ElementType.CModReqD:
+                case ElementType.FnPtr:
+                case ElementType.GenericInst:
+                case ElementType.MVar:
+                case ElementType.Pinned:
+                case ElementType.Ptr:
+                case ElementType.SzArray:
+                case ElementType.Sentinel:
+                case ElementType.Var:
+                    return true;
+            }
+
+            return false;
+        }
+
+        enum ElementType : byte
+        {
+            None = 0x00,
+            Void = 0x01,
+            Boolean = 0x02,
+            Char = 0x03,
+            I1 = 0x04,
+            U1 = 0x05,
+            I2 = 0x06,
+            U2 = 0x07,
+            I4 = 0x08,
+            U4 = 0x09,
+            I8 = 0x0a,
+            U8 = 0x0b,
+            R4 = 0x0c,
+            R8 = 0x0d,
+            String = 0x0e,
+            Ptr = 0x0f,   // Followed by <type> token
+            ByRef = 0x10,   // Followed by <type> token
+            ValueType = 0x11,   // Followed by <type> token
+            Class = 0x12,   // Followed by <type> token
+            Var = 0x13,   // Followed by generic parameter number
+            Array = 0x14,   // <type> <rank> <boundsCount> <bound1>  <loCount> <lo1>
+            GenericInst = 0x15,   // <type> <type-arg-count> <type-1> ... <type-n> */
+            TypedByRef = 0x16,
+            I = 0x18,   // System.IntPtr
+            U = 0x19,   // System.UIntPtr
+            FnPtr = 0x1b,   // Followed by full method signature
+            Object = 0x1c,   // System.Object
+            SzArray = 0x1d,   // Single-dim array with 0 lower bound
+            MVar = 0x1e,   // Followed by generic parameter number
+            CModReqD = 0x1f,   // Required modifier : followed by a TypeDef or TypeRef token
+            CModOpt = 0x20,   // Optional modifier : followed by a TypeDef or TypeRef token
+            Internal = 0x21,   // Implemented within the CLI
+            Modifier = 0x40,   // Or'd with following element types
+            Sentinel = 0x41,   // Sentinel for varargs method signature
+            Pinned = 0x45,   // Denotes a local variable that points at a pinned object
+
+            // special undocumented constants
+            Type = 0x50,
+            Boxed = 0x51,
+            Enum = 0x55
+        }
+
+        static TypeDefinition ResolveBaseType(TypeDefinition type)
+        {
+            if (type == null)
+            {
+                return null;
+            }
+
+            var baseType = type.BaseType;
+            if (baseType == null)
+            {
+                return null;
+            }
+
+            return baseType.Resolve();
+        }
+
+        public static MethodDefinition FindInterfaceMethodImplementation(TypeDefinition typeDefinition, MethodDefinition interfaceMethod, ref bool sawGenerics)
+        {
+            if (typeDefinition.HasGenericParameters)
+            {
+                sawGenerics = true;
+                return null;
+            }
+
+            var matching = GetMethod(typeDefinition.Methods, interfaceMethod);
+            if (matching != null)
+            {
+                return matching;
+            }
+
+            var baseType = ResolveBaseType(typeDefinition);
+            if (baseType != null)
+            {
+                return FindInterfaceMethodImplementation(baseType, interfaceMethod, ref sawGenerics);
+            }
+
+            return null;
+        }
+
+        public static MethodDefinition GetBaseMethod(MethodDefinition methodDefinition)
+        {
+            if (methodDefinition == null)
+            {
+                throw new ArgumentNullException("methodDefinition");
+            }
+
+            if (!methodDefinition.IsVirtual)
+            {
+                return null;
+            }
+
+            if (methodDefinition.IsNewSlot)
+            {
+                return null;
+            }
+
+            try
+            {
+                var baseType = ResolveBaseType(methodDefinition.DeclaringType);
+                while (baseType != null)
+                {
+                    var baseMethod = MetadataResolver.GetMethod(baseType.Methods, methodDefinition);
+                    if (baseMethod != null)
+                    {
+                        return baseMethod;
+                    }
+
+                    baseType = ResolveBaseType(baseType);
+                }
+            }
+            catch
+            {
+            }
+
+            return null;
+        }
+
+        private void CheckTypeDefinitions(string assemblyFullName, ModuleDefinition module, List<MemberReference> references)
+        {
+            var types = module.GetTypes();
+            foreach (var typeDef in types)
+            {
+                if (!typeDef.IsClass)
+                {
+                    continue;
+                }
+
+                if (typeDef.HasInterfaces)
+                {
+                    foreach (var interfaceImplementation in typeDef.Interfaces)
+                    {
+                        var interfaceTypeRef = interfaceImplementation.InterfaceType;
+                        references.Add(interfaceTypeRef);
+
+                        try
+                        {
+                            var interfaceTypeDef = interfaceTypeRef.Resolve();
+                            if (interfaceTypeDef != null)
+                            {
+                                if (interfaceTypeDef.HasMethods)
+                                {
+                                    foreach (var interfaceMethod in interfaceTypeDef.Methods)
+                                    {
+                                        if (interfaceMethod.ContainsGenericParameter)
+                                        {
+                                            // it's non-trivial to match when generics are involved
+                                            continue;
+                                        }
+
+                                        if (interfaceMethod.Name.Contains("GetConflictEditSpan"))
+                                        {
+                                        }
+
+                                        bool sawGenerics = false;
+                                        var matching = FindInterfaceMethodImplementation(typeDef, interfaceMethod, ref sawGenerics);
+                                        if (matching == null && !sawGenerics)
+                                        {
+                                            diagnostics.Add($"In assembly '{assemblyFullName}': Type {typeDef.FullName} does not implement interface method {interfaceMethod.FullName}");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+
+                if (typeDef.HasMethods)
+                {
+                    foreach (var methodDef in typeDef.Methods)
+                    {
+                        try
+                        {
+                            if (methodDef.HasOverrides)
+                            {
+                                foreach (var methodOverride in methodDef.Overrides)
+                                {
+                                    references.Add(methodOverride);
+                                }
+                            }
+
+                            var baseMethod = GetBaseMethod(methodDef);
+                            if (baseMethod != null)
+                            {
+                                var same = MetadataResolver.GetMethod(baseMethod.DeclaringType.Methods, methodDef);
+                                if (same == null)
+                                {
+                                    diagnostics.Add($"In assembly '{assemblyFullName}': Failed to find base method for '{methodDef.FullName}'");
+                                }
+                            }
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+            }
+        }
+
         private void CheckMembers(AssemblyDefinition assembly)
         {
             string assemblyFullName = assembly.Name.FullName;
             var module = assembly.MainModule;
-            var references = module.GetTypeReferences().Concat(module.GetMemberReferences());
+            var references = module.GetTypeReferences().Concat(module.GetMemberReferences()).ToList();
+
+            try
+            {
+                CheckTypeDefinitions(assemblyFullName, module, references);
+            }
+            catch
+            {
+            }
 
             foreach (var memberReference in references)
             {
