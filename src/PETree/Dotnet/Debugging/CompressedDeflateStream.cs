72921f84f8f000b120375a656ec32549698e8316
Kirill Osenkov <github@osenkov.com>
2025-01-18 12:59:32 -0800

    Move embedded Pdb decompression logic into EmbeddedPdb

--- src/PETree/Dotnet/Debugging/CompressedDeflateStream.cs
+++ src/PETree/Dotnet/Debugging/CompressedDeflateStream.cs
@@ -1,7 +1,4 @@
-﻿using System.IO;
-using System.IO.Compression;
-
-namespace GuiLabs.FileFormat.PE.Dotnet;
+﻿namespace GuiLabs.FileFormat.PE.Dotnet;
 
 public class CompressedDeflateStream : Node
 {
@@ -9,26 +6,4 @@ public class CompressedDeflateStream : Node
     {
         Text = "Compressed deflate stream";
     }
-
-    public override void Parse()
-    {
-        var bytes = Buffer.ReadBytes(Start, Length);
-        var compressedMemoryStream = new MemoryStream(bytes);
-        var deflateStream = new DeflateStream(compressedMemoryStream, CompressionMode.Decompress, leaveOpen: true);
-        var decompressedStream = new MemoryStream(DecompressedSize);
-        deflateStream.CopyTo(decompressedStream);
-
-        var metadataBuffer = new StreamBuffer(decompressedStream);
-        PdbMetadata = new Metadata
-        {
-            Buffer = metadataBuffer,
-            Start = 0,
-            Length = (int)decompressedStream.Length,
-            EmbeddedPdb = (EmbeddedPdb)Parent
-        };
-        PdbMetadata.Parse();
-    }
-
-    public int DecompressedSize { get; set; }
-    public Metadata PdbMetadata { get; set; }
 }
--- src/PETree/Dotnet/Debugging/EmbeddedPdb.cs
+++ src/PETree/Dotnet/Debugging/EmbeddedPdb.cs
@@ -1,4 +1,7 @@
-﻿namespace GuiLabs.FileFormat.PE.Dotnet;
+﻿using System.IO.Compression;
+using System.IO;
+
+namespace GuiLabs.FileFormat.PE.Dotnet;
 
 public class EmbeddedPdb : Node
 {
@@ -13,15 +16,32 @@ public class EmbeddedPdb : Node
 
         MPDB = AddFourBytes("MPDB header signature");
         DecompressedSize = AddFourBytes("Decompressed size");
+        var decompressedSize = DecompressedSize.Value;
         CompressedStream = new CompressedDeflateStream
         {
-            Length = length - 8,
-            DecompressedSize = DecompressedSize.Value
+            Length = length - 8
         };
         Add(CompressedStream);
+
+        var bytes = Buffer.ReadBytes(CompressedStream.Start, CompressedStream.Length);
+        var compressedMemoryStream = new MemoryStream(bytes);
+        var deflateStream = new DeflateStream(compressedMemoryStream, CompressionMode.Decompress, leaveOpen: true);
+        var decompressedStream = new MemoryStream(decompressedSize);
+        deflateStream.CopyTo(decompressedStream);
+
+        var metadataBuffer = new StreamBuffer(decompressedStream);
+        Metadata = new Metadata
+        {
+            Buffer = metadataBuffer,
+            Start = 0,
+            Length = (int)decompressedStream.Length,
+            EmbeddedPdb = this
+        };
+        Metadata.Parse();
     }
 
     public FourBytes MPDB { get; set; }
     public FourBytes DecompressedSize { get; set; }
     public CompressedDeflateStream CompressedStream { get; set; }
+    public Metadata Metadata { get; set; }
 }
